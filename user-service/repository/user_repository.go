4da8bcbe974f7a559c41634603a7fa2d159335fe
= <tovan.kamil@gmail.com>
2026-07-11 05:01:41 +0700

    split user interface into folder user_interface

--- user-service/repository/user_repository.go
+++ user-service/repository/user_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"micro-warehouse/user-service/interfaces"
 	"micro-warehouse/user-service/model"
 
 	"github.com/google/uuid"
@@ -16,23 +17,58 @@ USER ROLE : ASSIGN USER TO ROLE ,GET USER BY ROLE ID, GET ALL USER ROLE, EDIT AS
 
 */
 
-type userRepositoryInterface interface {
-	Createuser(ctx context.Context, user model.User) (*model.User, error)
-	GetAllUser(cxt context.Context, page int, limit int, search, sortBy, sortOrder string) ([]model.User, int64, error)
-	GetUserById(ctx context.Context, id uuid.UUID) (*model.User, error)
-	GetUserByEmail(ctx context.Context, email string) (*model.User, error)
-	UpdateUser(ctx context.Context, user model.User) error
-	DeleteUser(ctx context.Context, id uuid.UUID) error
-
-	GetUserByRole(ctx context.Context, roleName string) ([]model.User, error)
-
-	AssignUserToRole(ctx context.Context, user_id uuid.UUID, role_id uuid.UUID) error
-	EditAssignToRole(ctx context.Context, assignRoleId uuid.UUID, user_id uuid.UUID, role_id uuid.UUID) error
-	DeleteAssignToRole(ctx context.Context, assignRoleId uuid.UUID) error
-	GetAllUserToRoles(ctx context.Context, page, limit int, search, sortBy, sortOrder string) ([]model.UserRole, int64, error)
-	GetUserByRoleId(ctx context.Context, roleID uuid.UUID) ([]model.User, error)
-}
-
 type userRepository struct {
 	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) interfaces.UserRepositoryInterface {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) Createuser(ctx context.Context, user model.User) (*model.User, error) {
+	panic("unimplemented")
+}
+
+func (r *userRepository) GetAllUser(cxt context.Context, page int, limit int, search string, sortBy string, sortOrder string) ([]model.User, int64, error) {
+	panic("unimplemented")
+}
+
+func (r *userRepository) GetUserById(ctx context.Context, id uuid.UUID) (*model.User, error) {
+	panic("unimplemented")
+}
+
+func (r *userRepository) GetUserByEmail(ctx context.Context, email string) (*model.User, error) {
+	panic("unimplemented")
+}
+
+func (r *userRepository) UpdateUser(ctx context.Context, user model.User) error {
+	panic("unimplemented")
+}
+
+func (r *userRepository) DeleteUser(ctx context.Context, id uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (r *userRepository) GetUserByRole(ctx context.Context, roleName string) ([]model.User, error) {
+	panic("unimplemented")
+}
+
+func (r *userRepository) AssignUserToRole(ctx context.Context, user_id uuid.UUID, role_id uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (r *userRepository) EditAssignToRole(ctx context.Context, assignRoleId uuid.UUID, user_id uuid.UUID, role_id uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (r *userRepository) DeleteAssignToRole(ctx context.Context, assignRoleId uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (r *userRepository) GetAllUserToRoles(ctx context.Context, page int, limit int, search string, sortBy string, sortOrder string) ([]model.UserRole, int64, error) {
+	panic("unimplemented")
+}
+
+func (r *userRepository) GetUserByRoleId(ctx context.Context, roleID uuid.UUID) ([]model.User, error) {
+	panic("unimplemented")
 }
